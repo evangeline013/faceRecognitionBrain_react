c26cb38914204933f23579a554b5cde9fdbde108
Eva <evangeline013@gmail.com>
2018-12-17 15:07:02 -0500

    Add onChange, onClick, and Clarifai API call

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -10,8 +10,12 @@ class ImageLinkForm extends React.Component {
         </p>
         <div className='center'>
           <div className='form pa4 br3 shadow-5 center'>
-            <input className='f4 pa2 w-70 center' type='text' />
-            <button className='w-30 grow f4 link ph3 pv2 dib whit bg-light-purple'>Detect</button>
+            <input className='f4 pa2 w-70 center' type='text' onChange={this.props.onInputChange}/>
+            <button
+              className='w-30 grow f4 link ph3 pv2 dib whit bg-light-purple'
+              onClick={this.props.onButtonSubmit}>
+              Detect
+            </button>
           </div>
         </div>
       </div>
