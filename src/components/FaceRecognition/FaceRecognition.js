06ee7a2297e1d4b751cdea9e3fd89a2bfe567a40
Eva <evangeline013@gmail.com>
2018-12-17 20:35:26 -0500

    Add face detection box

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import './FaceRecognition.css';
 
 class FaceRecognition extends React.Component {
   render() {
+    const {imageUrl, box} = this.props;
     return (
       <div className='center ma'>
         <div className='absolute mt2'>
-          <img src={this.props.imageUrl} alt='' width='500px' height='auto'/>
+          <img id='inputimage' src={imageUrl} alt='' width='500px' height='auto'/>
+          <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
         </div>
       </div>
     );
